3bda4ba692db355371cb1809c020ab8c048e45f5
Sviatilishe <135019228+Sviatilishe@users.noreply.github.com>
2023-12-06 14:27:54 +0200

    Homework 23.API testing Data Models

--- tests/api/carsManagement/addCars.spec.js
+++ tests/api/carsManagement/addCars.spec.js
@@ -1,45 +1,37 @@
-import { test } from "../../../src/fixtures/UserGaragePage.fixture.js";
-import { expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { VALID_BRANDS_RESPONSE_BODY } from "../../../src/data/dict/brands.js";
 import { VALID_BRAND_MODELS } from "../../../src/data/dict/models.js";
 import { USERS } from "../../../src/data/dict/users.js";
-import pkg from "../../../playwright.config.api.cjs";
-const config = pkg.use;
 import APIClient from "../../../src/client/APIClient.js";
-import { getRandomNumber } from "../../../src/Utils/helpers.js";
+import CreateCarModel from "../../../src/models/cars/CreateCarModel.js";
 
-test.describe("POST", () => {
-  let client;
+test.describe('POST', ()=>{
+  let client
 
-  test.beforeAll(async () => {
-    client = await APIClient.authenticate(
-      { baseURL: process.env.API_URL },
-      {
-        email: USERS.JD.email,
-        password: USERS.JD.password,
-        remember: false,
-      },
-    );
-  });
+  test.beforeAll(async ()=>{
+      client = await APIClient.authenticate({
+          "email": USERS.JD.email,
+          "password": USERS.JD.password,
+          "remember": false
+      })
+  })
 
-  test("should create a car", async () => {
-    const brandId = VALID_BRANDS_RESPONSE_BODY.data[1].id;
-    const modelId = VALID_BRAND_MODELS[brandId].data[1].id;
+  test("should create a car using CreateCarModel", async () => {
+    const carModel = new CreateCarModel({carBrandId: 4, carModelId: 17, mileage: 3000})
+    const brand = VALID_BRANDS_RESPONSE_BODY.data.find((brand)=> brand.id === carModel.carBrandId)
+    const model = VALID_BRAND_MODELS[brand.id].data.find((model)=> model.id === carModel.carModelId)
+    const response = await client.cars.createCar(carModel)
 
-    const createRequestBody = {
-      carBrandId: brandId,
-      carModelId: modelId,
-      mileage: getRandomNumber(),
-    };
-
-    const createResponse = await client.cars.createCar(createRequestBody);
-    expect(createResponse.status, "Create Status code should be 201").toEqual(
-      201,
-    );
-    expect(createResponse.data.status).toBe("ok");
-    expect(
-      createResponse.data.data,
-      "Car should be created with data from request",
-    ).toMatchObject(createRequestBody);
+    const expectedBody = {
+        ...carModel,
+        initialMileage: carModel.mileage,
+        id: expect.any(Number),
+        carCreatedAt: expect.any(String),
+        updatedMileageAt: expect.any(String),
+        brand: brand.title,
+        model: model.title,
+        logo: brand.logoFilename
+    }
+    expect(response.data.data, 'Returned car object should be valid').toEqual(expectedBody)
   });
 });
--- tests/api/carsManagement/editCars.spec.js
+++ tests/api/carsManagement/editCars.spec.js
@@ -1,65 +1,70 @@
-import { test } from "../../../src/fixtures/UserGaragePage.fixture.js";
-import { expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { VALID_BRANDS_RESPONSE_BODY } from "../../../src/data/dict/brands.js";
 import { VALID_BRAND_MODELS } from "../../../src/data/dict/models.js";
 import { USERS } from "../../../src/data/dict/users.js";
-import pkg from "../../../playwright.config.api.cjs";
-const config = pkg.use;
 import APIClient from "../../../src/client/APIClient.js";
 import { getRandomNumber } from "../../../src/Utils/helpers.js";
+import CreateCarModel from "../../../src/models/cars/CreateCarModel.js";
+import EditCarModel from "../../../src/models/cars/EditCarModel.js";
 
-test.describe("PUT", () => {
+test.describe('POST', () => {
   let client;
+
   test.beforeAll(async () => {
-    client = await APIClient.authenticate(
-      { baseURL: process.env.API_URL },
-      {
-        email: USERS.JD.email,
-        password: USERS.JD.password,
-        remember: false,
-      },
-    );
+    client = await APIClient.authenticate({
+      "email": USERS.JD.email,
+      "password": USERS.JD.password,
+      "remember": false,
+    });
   });
-  test("should edit a car by returned id", async () => {
-    const brandId = VALID_BRANDS_RESPONSE_BODY.data[0].id;
-    const modelId = VALID_BRAND_MODELS[brandId].data[0].id;
 
-    const createRequestBody = {
-      carBrandId: brandId,
-      carModelId: modelId,
+  test("should edit created car using EditCarModel", async () => {
+    const carModel = new CreateCarModel({carBrandId: 4, carModelId: 17, mileage: 1})
+    const brand = VALID_BRANDS_RESPONSE_BODY.data.find((brand)=> brand.id === carModel.carBrandId)
+    const model = VALID_BRAND_MODELS[brand.id].data.find((model)=> model.id === carModel.carModelId)
+    const response = await client.cars.createCar(carModel)
+
+    const expectedBody = {
+        ...carModel,
+        initialMileage: carModel.mileage,
+        id: expect.any(Number),
+        carCreatedAt: expect.any(String),
+        updatedMileageAt: expect.any(String),
+        brand: brand.title,
+        model: model.title,
+        logo: brand.logoFilename
+    }
+    expect(response.data.data, 'Returned car should be valid').toEqual(expectedBody)
+    
+    const id = response.data.data.id;
+    const editRequestBody = new EditCarModel({
+      carId: id,
       mileage: getRandomNumber(),
-    };
-
-    const createResponse = await client.cars.createCar(createRequestBody);
-    expect(createResponse.status, "Create Status code should be 201").toEqual(
-      201,
-    );
-    expect(createResponse.data.status).toBe("ok");
-    expect(
-      createResponse.data.data,
-      "Car should be created with data from request",
-    ).toMatchObject(createRequestBody);
-
-    const id = createResponse.data.data.id;
-
-    const newBrandId = VALID_BRANDS_RESPONSE_BODY.data[1].id;
-    const newModelId = VALID_BRAND_MODELS[newBrandId].data[0].id;
-    const editRequestBody = {
-      carBrandId: newBrandId,
-      carModelId: newModelId,
-      mileage: getRandomNumber(),
-    };
+    });
 
     const editResponse = await client.cars.editUserCar(id, editRequestBody);
+
+    console.log("Edit Response:", editResponse.status, editResponse.data);
+
     expect(editResponse.status, "Edit Status code should be 200").toEqual(200);
     expect(editResponse.data.status).toBe("ok");
-    expect(
-      editResponse.data.data,
-      "Car should be edited with data from request",
-    ).toMatchObject({
-      carBrandId: newBrandId,
-      carModelId: newModelId,
-      ...editRequestBody,
+
+    const editedCar = editResponse.data.data;
+
+    expect(editedCar).toMatchObject({
+      id: expect.any(Number),
+      initialMileage: expect.any(Number),
+      carCreatedAt: expect.any(String),
+      updatedMileageAt: expect.any(String),
+      brand: expect.any(String),
+      model: expect.any(String),
+      logo: expect.any(String),
+    });
+    
+    expect(editedCar).toMatchObject({
+      carBrandId: carModel.carBrandId, 
+      carModelId: carModel.carModelId,
+      mileage: editRequestBody.mileage,
     });
   });
 });
